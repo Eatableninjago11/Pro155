969d3514e752e45c5c1dad78b2aa7b5f4d6fe53d
Eatableninjago11 <83182611+Eatableninjago11@users.noreply.github.com>
2022-07-13 13:00:02 -0500

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,20 @@
+AFRAME.registerComponent("game-play", {
+    schema: {
+      elementId: { type: "string", default: "#coin" },
+    },
+  
+    update: function () {
+      this.isCollided(this.data.elementId);
+    },
+  
+    isCollided: function (elementId) {
+      const element = document.querySelector(elementId);
+      element.addEventListener("collide", (e) => {
+        if (elementId.includes("#ring")) {
+          console.log(elementId + " collision");
+        } else if (elementId.includes("#hurdle")) {
+          console.log("bird collision");
+        }
+      });
+    },
+  });
